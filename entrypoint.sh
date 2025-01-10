5b4b7e1fa4d460d54a11fa96a5a4106bae1527b8
Brian Vo <datvo2k@gmail.com>
2025-01-11 01:57:28 +0700

    Remove deprecated configuration files and add new 3proxy configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,3 +22,11 @@ if [[ $? -ne 0 ]]; then
 fi
 
 echo "Modification complete. Removed '| cmd \$iptables-restore -n' from $WG_QUICK_PATH. Wireguard should now work."
+
+# Start WireGuard
+wg-quick up /etc/wireguard/wg0.conf
+
+# Wait for WireGuard interface to be up
+sleep 1
+
+exec /usr/local/bin/3proxy /etc/3proxy/3proxy.cfg
